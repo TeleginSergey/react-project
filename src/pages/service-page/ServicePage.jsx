d06ed2396e543630867ae95ede6b7503730f6e0c
Serzh <telegins125@gmail.com>
2024-11-30 13:30:54 +0300

    feat: add page of services

--- src/pages/service-page/ServicePage.jsx
+++ src/pages/service-page/ServicePage.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {Card} from "@consta/uikit/Card";
+import {Text} from "@consta/uikit/Text";
+import './ServicePage.css';
+import { Grid, GridItem } from '@consta/uikit/Grid';
+
 
 const ServicePage = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('https://673423afa042ab85d1190055.mockapi.io/api/v1/services');
+            const data = await response.json();
+            setData(data);
+        }
+        fetchData();
+    }, []);
+
     return (
-        <div>
-            ServicePage
+        <div className="card-container">
+            <Grid gap={"m"} cols={3}>
+                {data.map((card, index) => (
+                <GridItem>
+                    <Card key={index} verticalSpace="m" horizontalSpace="xl" form="round" className="rounded-card-main">
+                        <Text size="m" weight="bold" align="center">{card.name}</Text>
+                        <Text align="left">{card.description}</Text>
+                    </Card>
+                </GridItem>
+            ))}
+            </Grid>
         </div>
-    )
+    );
 }
 
 export default ServicePage;
