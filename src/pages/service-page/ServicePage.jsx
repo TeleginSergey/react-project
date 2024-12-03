45bec63c7bac4d7962ce4c768db4f242c91eb86c
Serzh <telegins125@gmail.com>
2024-12-03 11:35:02 +0300

    feat: add pictures into the cards

--- src/pages/service-page/ServicePage.jsx
+++ src/pages/service-page/ServicePage.jsx
@@ -1,25 +1,24 @@
-
-import {Card} from "@consta/uikit/Card";
-import {Text} from "@consta/uikit/Text";
+import { Card } from "@consta/uikit/Card";
+import { Text } from "@consta/uikit/Text";
 import { Grid, GridItem } from '@consta/uikit/Grid';
-import {Loader} from "@consta/uikit/Loader";
-import {useSelector} from "react-redux";
-import {Link} from "react-router-dom";
+import { Loader } from "@consta/uikit/Loader";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
+import { useApiQuery } from "../../store/api-actions";
+import { setServices } from "../../store/store";
 import './ServicePage.css';
-import {useApiQuery} from "../../store/api-actions";
-import {setServices} from "../../store/store";
-
 
 const ServicePage = () => {
     const services = useSelector((state) => state.services);
     useApiQuery("https://673423afa042ab85d1190055.mockapi.io/api/v1/services", setServices);
 
-
     if (services.loading) {
-        return <div style={{display: "flex", alignItems: 'center', justifyContent: 'center', height: '70vh'}}>
-            <Loader size={'m'}/>
-        </div>;
+        return (
+            <div style={{ display: "flex", alignItems: 'center', justifyContent: 'center', height: '70vh' }}>
+                <Loader size={'m'} />
+            </div>
+        );
     }
 
     if (services.error) {
@@ -33,9 +32,10 @@ const ServicePage = () => {
                     <GridItem key={index}>
                         <Card verticalSpace="m" horizontalSpace="xl" form="round" className="rounded-card-main">
                             <Link to={`http://localhost:3000/service/${card.id}`}>
-                                <Text size="m" weight="bold" align="center">{card.name}</Text>
+                                <Text size="m" weight="bold" align="center" style={{ lineHeight: '1.2' }}>{card.name}</Text>
                             </Link>
-                            <Text align="left">{card.description}</Text>
+                            {card.image && <img src={card.image} alt={card.name} className="service-detail-image" />}
+                            <Text align="left" style={{ lineHeight: '1.2' }}>{card.description}</Text>
                         </Card>
                     </GridItem>
                 ))}
